644b0ff92e5a5c58082f0175d71393884a57365b
VitoWidjojo <geometrydashcitizen@gmail.com>
2026-06-25 00:06:54 +0700

    changed login page a bit

--- Frontend/app/Http/Controllers/LoginController.php
+++ Frontend/app/Http/Controllers/LoginController.php
@@ -11,11 +11,6 @@ class LoginController extends Controller
     public function getUsers(){
         $data = Http::get('http://127.0.0.1:8003/api/users');
         $users = $data->json();
-
-        for($i = 0; $i < count($users['users']); $i++){
-            echo($users['users'][$i]['user_name']);
-        }
-        echo('<br>');
         return view('login', ['users' => $users['users']]);
     }
 }
