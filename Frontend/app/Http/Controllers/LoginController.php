2ff5c4f7d6a3ff367252c13b6a5151281fce6e46
VitoWidjojo <geometrydashcitizen@gmail.com>
2026-06-25 04:00:24 +0700

    added middleware

--- Frontend/app/Http/Controllers/LoginController.php
+++ Frontend/app/Http/Controllers/LoginController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Http;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
     //
-    public function getUsers(){
+    public function index(){
         $data = Http::get('http://127.0.0.1:8003/api/users');
         $users = $data->json();
         return view('login', ['users' => $users['users']]);
+    }
+
+    public function authenticateUser(Request $request){
+        // Authentication logic here
+        $credentials = $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            $request->session()->regenerate();
+            return redirect()->intended('/home');
+        }
+
+        // Authentication failed...
+        return back()->withErrors([
+            'username' => 'The provided credentials do not match our records.',
+        ]);
     }
 }
