5a92b44cbce116b7ac095020a17f0cfb35fab493
C14240018 <C14240018@john.petra.ac.id>
2026-06-25 22:37:55 +0700

    double Auth glitch

--- Frontend/app/Http/Controllers/AuthController.php
+++ Frontend/app/Http/Controllers/AuthController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\Http;
 
 class AuthController extends Controller
 {
