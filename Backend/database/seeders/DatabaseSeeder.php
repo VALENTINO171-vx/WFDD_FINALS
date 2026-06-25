05d7e2e2f813f9dc90bcd47a7780d40b8268f60b
C14240018 <C14240018@john.petra.ac.id>
2026-06-25 13:57:13 +0700

    Tambahkan supaya bisa langsung db:seed

---
+++ Backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+//supaya bisa db:seed langsung
+{
+    use WithoutModelEvents;
+    public function run(): void
+    {
+        $this->call([
+            UserSeeder::class,
+        ]);
+        $this->call([RestaurantSeeder::class]);
+    }
+}
